f85434a664393b143631f4e2fec6f2ec8851f287
timur1608 <timur1608@list.ru>
2024-10-27 21:52:59 +0300

    add logging for all classes with log4j2

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -2,20 +2,28 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
 import static java.util.Objects.isNull;
 
 public class Hippodrome {
 
     private final List<Horse> horses;
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public Hippodrome(List<Horse> horses) {
         if (isNull(horses)) {
+            LOGGER.error("Horses list is null");
             throw new IllegalArgumentException("Horses cannot be null.");
         } else if (horses.isEmpty()) {
+            LOGGER.error("Horses list is empty");
             throw new IllegalArgumentException("Horses cannot be empty.");
         }
 
         this.horses = horses;
+        LOGGER.debug("Создание Hippodrome, лошадей [{}]", horses.size());
     }
 
     public List<Horse> getHorses() {
